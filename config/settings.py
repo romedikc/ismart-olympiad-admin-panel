579e867f733cfc1cd6beb1d6b2390b34a9f6a21e
yountuan <yountuan@Aidas-MacBook-Air.local>
2024-04-24 01:27:10 +0600

    deployed

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -94,6 +95,8 @@ DATABASES = {
     }
 }
 
+database_url = config("database_url")
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
