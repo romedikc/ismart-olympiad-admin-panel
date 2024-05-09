3cdb3c8dcb734aef960009fccde76b2408d53a43
yountuan <yountuan@Aidas-MacBook-Air.local>
2024-05-10 01:47:03 +0600

    added str for partcipant

--- apps/teams/models.py
+++ apps/teams/models.py
@@ -9,7 +9,7 @@ class Participant(models.Model):
     phone_number = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
-        return self.first_name
+        return f'{self.first_name} {self.last_name} '
 
 
 class Team(models.Model):
